3d0e7fd566f59a84b27c7cb21ef33366e5751429
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-05-15 19:26:13 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'db.php';
 
-
 function getItemCode($itemName) {
     $mapping = [
         "Camera"     => "CAM-004",
@@ -15,7 +14,32 @@ function getItemCode($itemName) {
     return isset($mapping[$itemName]) ? $mapping[$itemName] : null;
 }
 
+function getItemName($itemCode) {
+    $mapping = [
+        "CAM-004" => "Camera",
+        "GEN-005" => "Generator",
+        "LTP-003" => "Laptop",
+        "PRJ-001" => "Projector",
+        "SND-002" => "Sound System"
+    ];
+    
+    return isset($mapping[$itemCode]) ? $mapping[$itemCode] : "Unknown";
+}
 
+function getRentalRate($conn, $itemCode) {
+    $stmt = $conn->prepare("SELECT rental_rate_per_day FROM rental_items WHERE item_code = ?");
+    $stmt->bind_param("s", $itemCode);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $rate = null;
+
+    if ($row = $result->fetch_assoc()) {
+        $rate = $row['rental_rate_per_day'];
+    }
+
+    $stmt->close();
+    return $rate;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Sanitize inputs
@@ -23,12 +47,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $first = htmlspecialchars($_POST['first_name']);
   $last = htmlspecialchars($_POST['last_name']);
   $duration = intval($_POST['duration']);
-  $item = htmlspecialchars($_POST['item']);
+  $itemName = htmlspecialchars($_POST['item']);
   $item = getItemCode($itemName);
   $phone = htmlspecialchars($_POST['phone_number']);
 
   // Connect to DB
   $conn = getDBConnection();
+
+  // Get rental rate per day for this item code
+  $rentalRate = getRentalRate($conn, $item);
+  if ($rentalRate === null) {
+    die("Error: Could not retrieve rental rate for item.");
+  }
+
+  $cost = $rentalRate * $duration;
 
   // Insert into DB
   $stmt = $conn->prepare("INSERT INTO rental_requests 
@@ -68,7 +100,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <b>Name:</b> <?= $first . ' ' . $last ?><br>
       <b>Phone:</b> <?= $phone ?><br>
       <b>Duration:</b> <?= $duration ?> day(s)<br>
-      <b>Item:</b> <?= $item ?>
+      <b>Item:</b> <?= getItemName($item) ?><br>
+      <b>Rental Rate per Day:</b> <?= number_format($rentalRate, 2) ?><br>
+      <b>Total Cost:</b> <?= number_format($cost, 2) ?>
     </div>
     <a href="index.php" class="submit-btn" style="text-align:center; display:block;">Back to Form</a>
   </div>
